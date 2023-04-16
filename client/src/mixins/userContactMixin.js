ed66a080eee4ed5a7c225520967a23c0c3d2fe6a
Alex Pepper <alexander.pepper@gmail.com>
2023-04-16 13:27:39 -0500

    Make progress dockerizing for dev

--- client/src/mixins/userContactMixin.js
+++ client/src/mixins/userContactMixin.js
@@ -1,4 +1,15 @@
-import { formattedPhone } from '../../../app/utilities/user.utilities'
+// TODO: This function came from user.utilities.js,
+//  but have been duplicated to support dockerization.
+//  Eventually, it'd be ideal to keep a shared folder.
+const formattedPhone = user => {
+  if (user.phone) {
+    if (user.isInternationalPhone) {
+      return `+${user.phone}`
+    } else {
+      return `(${user.phone.substring(0, 3)}) ${user.phone.substring(3, 6)}-${user.phone.substring(6)}`
+    }
+  }
+}
 
 export default {
   props: {
